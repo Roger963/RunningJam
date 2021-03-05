25768f88ed3fc371dcf2f624bb89b596dd15adee
kevin1125 <77078505+kevin1125@users.noreply.github.com>
2021-03-05 13:29:10 -0500

    4

--- src/Preloader.js
+++ src/Preloader.js
@@ -1,15 +1,15 @@
 Space.Preloader = function(game){
-	// define width and height of the game
+	// definir ancho y alto del juego
 	Space.GAME_WIDTH = 1055;
 	Space.GAME_HEIGHT = 703;
 };
 Space.Preloader.prototype = {
 	preload: function(){
-		// set background color and preload image
+		// establecer el color de fondo y la imagen de precarga
 		this.stage.backgroundColor = '#B4D9E7';
 		this.preloadBar = this.add.sprite((Space.GAME_WIDTH-311)/2, (Space.GAME_HEIGHT-27)/2, 'preloaderBar');
 		this.load.setPreloadSprite(this.preloadBar);
-		// load images
+		// cargar imágenes
 		this.load.image('fondo', 'img/fondo.png');
 		this.load.image('estrellas', 'img/estrellas.png');
 		this.load.image('astronauta', 'img/astronauta.png');
@@ -17,13 +17,13 @@ Space.Preloader.prototype = {
 		this.load.image('game-over', 'img/game-over.png');
 		this.load.image('score-bg', 'img/score-bg.png');
 		this.load.image('button-pause', 'img/button-pause.png');
-		// load spritesheets
+		// cargar imagenes
 		this.load.spritesheet('enemy', 'img/enemy.png', 82, 98);
 		this.load.spritesheet('monster-idle', 'img/monster-idle.png', 103, 131);
 		this.load.spritesheet('play', 'img/play.png', 401, 143);
 	},
 	create: function(){
-		// start the MainMenu state
+		// iniciar el estado MainMenu
 		this.state.start('MainMenu');
 	}
 };
