bde71c0259dfc106c51374d2bc77e23fb0de11da
AlexisPj <76967417+AlexisPj@users.noreply.github.com>
2021-03-05 12:04:16 -0500

    1

--- src/Boot.js
+++ src/Boot.js
@@ -2,17 +2,17 @@ var Space = {};
 Space.Boot = function(game){};
 Space.Boot.prototype = {
 	preload: function(){
-		// preload the loading indicator first before anything else
+		// Cargue el indicador de carga primero, antes que cualquier cosa
 		this.load.image('preloaderBar', 'img/loading-bar.png');
 	},
 	create: function(){
-		// set scale options
+		// Establecemos las opciones de escala
 		this.input.maxPointers = 1;
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
 		this.scale.setScreenSize(true);
-		// start the Preloader state
+		// Se inicia la escena de precargador o preload
 		this.state.start('Preloader');
 	}
 };
