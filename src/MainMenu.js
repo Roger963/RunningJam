04550f1f4e0783596a2ca617b41eeb63c7338f89
kevin1125 <77078505+kevin1125@users.noreply.github.com>
2021-03-05 13:23:32 -0500

    3

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,18 +1,18 @@
 Space.MainMenu = function(game){};
 Space.MainMenu.prototype = {
 	create: function(){
-		// display images
+		// mostrar imágenes
 		this.add.sprite(0, 0, 'fondo');
 		this.add.sprite(0,0, 'estrellas');
 		
 		this.add.sprite(90, Space.GAME_HEIGHT-414, 'astronauta');
 		this.add.sprite((Space.GAME_WIDTH-820)/2, -80, 'RunningJam');
-		// add the button that will start the game
+		// agrega el botón que iniciará el juego
 		this.add.button(Space.GAME_WIDTH-600-10, Space.GAME_HEIGHT-280-10, 'play', this.startGame, this, 1, 0, 2);
-		//this.add.button(Candy.GAME_WIDTH-401-10, Candy.GAME_HEIGHT-143-10, 'button-start', this.startGame, this, 1, 0, 2);
+		//this.add.button(Space.GAME_WIDTH-401-10, Space.GAME_HEIGHT-143-10, 'button-start', this.startGame, this, 1, 0, 2);
 	},
 	startGame: function() {
-		// start the Game state
+		// iniciar la escena del juego
 		this.state.start('Game');
 	}
 };
